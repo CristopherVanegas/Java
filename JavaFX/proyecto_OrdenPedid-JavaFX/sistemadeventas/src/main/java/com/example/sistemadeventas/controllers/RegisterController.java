1b70bfd53e3b1c89a626282021dcf7004438c75f
Cristopher Vanegas <itsmrjkali21@Cristophers-MacBook-Pro.local>
2024-01-14 09:00:10 -0500

    Bug when adding new users, add to the end but it does not validate if exists, also overwrite clientes

--- JavaFX/proyecto_OrdenPedid-JavaFX/sistemadeventas/src/main/java/com/example/sistemadeventas/controllers/RegisterController.java
+++ JavaFX/proyecto_OrdenPedid-JavaFX/sistemadeventas/src/main/java/com/example/sistemadeventas/controllers/RegisterController.java
@@ -15,11 +15,12 @@ import com.example.sistemadeventas.models.Cliente;
 import com.example.sistemadeventas.models.ClienteMayorista;
 import com.example.sistemadeventas.models.ClienteMinorista;
 import com.example.sistemadeventas.view.App;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.type.CollectionType;
 
 public class RegisterController {
+
     @FXML
     private TextField cedulaRUCField;
 
@@ -49,21 +50,38 @@ public class RegisterController {
 
     private ToggleGroup tipoClienteToggleGroup;
 
+    // Listas para almacenar clientes, minoristas y mayoristas
+    private List<Cliente> listaClientes = new ArrayList<>();
+    private List<ClienteMinorista> listaMinoristas = new ArrayList<>();
+    private List<ClienteMayorista> listaMayoristas = new ArrayList<>();
+
+    private static final String CLIENTES_JSON_PATH = "sistemadeventas/src/main/java/com/example/sistemadeventas/data/clientes.json";
+    private static final String MAYORISTAS_JSON_PATH = "sistemadeventas/src/main/java/com/example/sistemadeventas/data/mayoristas.json";
+    private static final String MINORISTAS_JSON_PATH = "sistemadeventas/src/main/java/com/example/sistemadeventas/data/minoristas.json";
+
     @FXML
     private void initialize() {
         tipoClienteToggleGroup = new ToggleGroup();
         minoristaRadio.setToggleGroup(tipoClienteToggleGroup);
         mayoristaRadio.setToggleGroup(tipoClienteToggleGroup);
-    }
 
-    private static final String CLIENTES_JSON_PATH = "src/main/java/com/example/sistemadeventas/data/clientes.json";
-    private static final String MAYORISTAS_JSON_PATH = "src/main/java/com/example/sistemadeventas/data/mayoristas.json";
-    private static final String MINORISTAS_JSON_PATH = "src/main/java/com/example/sistemadeventas/data/minoristas.json";
+        // Leer datos existentes de los archivos y cargar las listas
+        listaClientes = leerClientesDesdeArchivo(CLIENTES_JSON_PATH, Cliente.class);
+        listaMinoristas = leerClientesDesdeArchivo(MINORISTAS_JSON_PATH, ClienteMinorista.class);
+        listaMayoristas = leerClientesDesdeArchivo(MAYORISTAS_JSON_PATH, ClienteMayorista.class);
+    }
 
     @FXML
     private void handleRegister() throws IOException {
         if (camposLlenos() && tipoClienteSeleccionado()) {
             String cedulaRUC = cedulaRUCField.getText();
+
+            // Verificar si el cliente ya existe
+            if (clienteYaExiste(cedulaRUC)) {
+                mostrarError("El cliente con el mismo Cédula/RUC ya está registrado.");
+                return;
+            }
+
             String nombres = nombresField.getText();
             String apellidos = apellidosField.getText();
             String correo = correoField.getText();
@@ -73,43 +91,61 @@ public class RegisterController {
             String tipoCliente = minoristaRadio.isSelected() ? "Minorista" : "Mayorista";
 
             // Crear objeto Cliente, ClienteMayorista o ClienteMinorista según el tipo
+            Cliente clienteGeneral;
             if ("Mayorista".equals(tipoCliente)) {
                 ClienteMayorista mayorista = new ClienteMayorista(cedulaRUC, nombres, apellidos, correo, direccion,
                         telefono, "Persona de Contacto");
-                guardarClienteEnArchivo(mayorista, MAYORISTAS_JSON_PATH);
+                listaMayoristas.add(mayorista);
+                clienteGeneral = mayorista;
             } else {
                 ClienteMinorista minorista = new ClienteMinorista(cedulaRUC, nombres, apellidos, correo, direccion,
                         telefono, 0);
-                guardarClienteEnArchivo(minorista, MINORISTAS_JSON_PATH);
+                listaMinoristas.add(minorista);
+                clienteGeneral = minorista;
             }
 
-            // Guardar en el archivo clientes.json
-            Cliente clienteGeneral = new Cliente(cedulaRUC, nombres, apellidos, correo, direccion, telefono);
+            // Agregar el nuevo cliente a la lista general
+            listaClientes.add(clienteGeneral);
 
-            // Usar un identificador único basado en el timestamp
-            String uniqueIdentifier = cedulaRUC + "_" + System.currentTimeMillis();
-            clienteGeneral.setCodigoCliente(uniqueIdentifier);
+            // Agregar el nuevo cliente a la lista específica (minoristas o mayoristas)
+            if ("Mayorista".equals(tipoCliente)) {
+                listaMayoristas.add((ClienteMayorista) clienteGeneral);
+            } else {
+                listaMinoristas.add((ClienteMinorista) clienteGeneral);
+            }
 
-            guardarClienteEnArchivo(clienteGeneral, CLIENTES_JSON_PATH);
+            // Guardar en el archivo correspondiente (lista específica)
+            if ("Mayorista".equals(tipoCliente)) {
+                guardarClienteEnArchivo(clienteGeneral, MAYORISTAS_JSON_PATH, listaMayoristas);
+            } else {
+                guardarClienteEnArchivo(clienteGeneral, MINORISTAS_JSON_PATH, listaMinoristas);
+            }
+
+            // Guardar en el archivo de clientes generales
+            guardarClienteEnArchivo(clienteGeneral, CLIENTES_JSON_PATH, listaClientes);
 
             System.out.println("Cliente registrado con éxito");
+
+            // Cambiar a la vista de inicio de sesión
             App.setRoot("login");
         } else {
             mostrarError("Por favor, llene todos los campos y seleccione el tipo de cliente");
         }
     }
 
-    private void guardarClienteEnArchivo(Cliente cliente, String filePath) {
+    @FXML
+    private void handleCancel() throws IOException {
+        App.setRoot("login");
+    }
+
+    private <T> void guardarClienteEnArchivo(Cliente cliente, String filePath, List<T> listaClientes) {
+        // Método para guardar el cliente en el archivo
         try {
-            // Lee los clientes existentes del archivo
-            List<Cliente> clientesExistentes = leerClientesDesdeArchivo(filePath);
+            File file = new File(filePath);
 
-            // Agrega el nuevo cliente a la lista
-            clientesExistentes.add(cliente);
-
-            // Guarda la lista actualizada en el archivo
+            // Guarda la lista específica en el archivo
             ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
-            objectMapper.writeValue(new File(filePath), clientesExistentes);
+            objectMapper.writeValue(file, listaClientes);
 
             System.out.println("Guardado con éxito en la ruta: " + filePath);
         } catch (IOException e) {
@@ -118,8 +154,9 @@ public class RegisterController {
         }
     }
 
-    private List<Cliente> leerClientesDesdeArchivo(String filePath) {
-        List<Cliente> clientes = new ArrayList<>();
+    private <T> List<T> leerClientesDesdeArchivo(String filePath, Class<T> type) {
+        // Método para leer clientes desde el archivo
+        List<T> clientes = new ArrayList<>();
 
         try {
             File file = new File(filePath);
@@ -129,8 +166,14 @@ public class RegisterController {
                 ObjectMapper objectMapper = new ObjectMapper();
 
                 // Lee la lista de clientes desde el archivo
-                clientes = objectMapper.readValue(file, new TypeReference<List<Cliente>>() {
-                });
+                CollectionType collectionType = objectMapper.getTypeFactory().constructCollectionType(List.class, type);
+                clientes = objectMapper.readValue(file, collectionType);
+
+                System.out.println("Leído con éxito desde el archivo: " + filePath);
+                System.out.println("Lista de clientes leídos:");
+                clientes.forEach(System.out::println);
+            } else {
+                System.out.println("Archivo no encontrado: " + filePath);
             }
         } catch (IOException e) {
             System.err.println("Error al leer el archivo: " + filePath);
@@ -138,11 +181,6 @@ public class RegisterController {
         }
 
         return clientes;
-    }
-
-    @FXML
-    private void handleCancel() throws IOException {
-        App.setRoot("login");
     }
 
     private boolean camposLlenos() {
@@ -160,6 +198,10 @@ public class RegisterController {
 
     private void mostrarError(String mensaje) {
         errorLabel.setText(mensaje);
-        errorLabel.setStyle("-fx-text-fill: red;");
+    }
+
+    private boolean clienteYaExiste(String cedulaRUC) {
+        // Verificar si el cliente ya existe en la lista general
+        return listaClientes.stream().anyMatch(cliente -> cliente.getCedulaRUC().equals(cedulaRUC));
     }
 }
